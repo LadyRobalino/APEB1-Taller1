0737a342453391b1f82bfb298051306f5438e46a
Lady Robalino <lyrobalino1@utpl.edu.ec>
2023-10-24 02:17:26 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,18 @@
+
+package proyecto02;
+
+/**
+ *
+ * @author ladychan
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además se usar GitHubClassroom");
+        
+    }
+    
+}
