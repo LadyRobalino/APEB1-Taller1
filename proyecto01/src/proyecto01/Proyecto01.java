eef025c6ec0c442ed49f1e7d9b07d79159db7a83
Lady Robalino <lyrobalino1@utpl.edu.ec>
2023-10-24 02:48:29 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,7 +14,7 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde Git Creado por LadyRobalino");
     }
     
 }
